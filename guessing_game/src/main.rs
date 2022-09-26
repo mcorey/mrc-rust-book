0a4bdd1ce4a84da87556592a653fd12f1c4f2a26
Michael R Corey <michael.corey@gmail.com>
2022-09-26 16:22:59 -0400

    Commit for guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,35 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    // println!("The secret number is: {secret_number}");
+    loop{
+        println!("Please Input Your Guess");
+
+        let mut guess = String::new(); //mut makes it mutable; = is the assignment of the rhs to the variable (lhs)
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You Guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => {
+                println!("Winner, Winner, Chicken Dinner!");
+                break;
+            }
+        }
+    }
 }
